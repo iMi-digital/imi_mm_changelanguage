b7aef12eb431bd21d015e505fccc35091dfa6a37
Alexander Menk <a.menk@imi.de>
2020-04-04 16:35:29 +0200

    Drop Deprecated Changelanguage V2 Support

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,3 +1,2 @@
 <?php
-$GLOBALS['TL_HOOKS']['translateUrlParameters'][] = array('iMi\MMChangeLanguage\ImiMMChangeLanguageObserver', 'translateMMUrlsV2');
 $GLOBALS['TL_HOOKS']['changelanguageNavigation'][] = array('iMi\MMChangeLanguage\ImiMMChangeLanguageObserver', 'translateMMUrlsV3');
